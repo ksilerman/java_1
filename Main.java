484b76370a5fbc5bab09a63f1373af56ce3d1ce4
ksilar <44767399+ksilerman@users.noreply.github.com>
2018-11-19 22:30:12 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package java_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+    byte bt, short sh, int , long, float, double, char, boolean;
+	int a, b, c, d
+    }
+    static int task1 (int a, int b, int c,int d) {
+        return a * (b + (c / d))
+    }
+
+}
