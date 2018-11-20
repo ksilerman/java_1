ad189fef4ee411737a3e20b18f2990123a27f64a
ksilar <ksilar@mail.ru>
2018-11-20 12:41:38 +0300

    task1 yes!

--- Main.java
+++ Main.java
@@ -3,11 +3,18 @@ package java_1;
 public class Main {
 
     public static void main(String[] args) {
-    byte bt, short sh, int , long, float, double, char, boolean;
-	int a, b, c, d
+   // byte bt, short sh, int , long, float, double, char, boolean;
+	int a = 2;
+    int b = 4;
+    int c = 8;
+    int d = 16;
+	System.out.println("a * (b + (c / d)) =" + task1(a, b, c, d)); //результат 8
+
+
     }
-    static int task1 (int a, int b, int c,int d) {
-        return a * (b + (c / d))
+
+    public static int task1 (int a, int b, int c, int d) {
+        return a * (b + (c / d));
     }
 
 }
